73a2cb795a581049a7581af26bf762546ca4a32f
franck <g-gongyan@tedu.cn>
2020-07-26 01:02:15 +0800

    test

---
+++ Packages/__init__.py
@@ -0,0 +1,7 @@
+
+# 此类写法可以在外部直接通过 from Package import 函数
+# from .module1 import moudule1
+# from .module2 import moudule2
+
+# 定义在模块倒入的时候，加载的变量，里边是字符串
+__all__ = ['module1', 'module2', 'subpackage']
--- python_moudule.py
+++ python_moudule.py
@@ -2,7 +2,21 @@
  this is for python module  test!
 """
 
-import Packages
+# from Packages.module1 import moudule1
+
+#print('this is start import [from Packages.module1 import moudule1]')
+#moudule1()
+
+# from Packages import moudule2,moudule1
+
+# moudule1()
+# print('---'*20)
+# moudule2()
 
 
-Packages.module1.moudule1()
+
+from Packages import * 
+# just import __all__ list variable!
+print(dir())
+
+module1.moudule1()
