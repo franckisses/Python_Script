6a004054cda1c64d202b5c8d9141f0654b701269
franck <franck_gxu@outlook.com>
2020-07-31 04:11:32 +0800

    test state

---
+++ state_test.py
@@ -0,0 +1,28 @@
+
+from state import curr,switch,stateful,State,behavior
+
+
+@stateful
+class People(object): 
+    class Workday(State):
+        default = True
+        @behavior
+        def day(self):
+            print('work hard')
+    
+    class Weekend(State):
+        @behavior
+        def day(self):
+            print('play hard')
+
+people = People()
+
+while True:
+    for i in range(1,8):
+        if i == 6:
+            switch(people,People.Weekend)
+        if i == 1:
+            switch(people,People.Workday)
+    people.day()
+
+# 是python 2  中的方法。python3 不适用，可以理解思想
